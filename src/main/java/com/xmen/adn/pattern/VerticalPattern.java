7823fa40b50ea0632cf096ba81b8e350c9ae1b3b
Diego Zarate <diego.zarate@360txp.net>
2015-06-09 17:59:07 -0500

    adding vertical pattern search

--- src/main/java/com/xmen/adn/pattern/VerticalPattern.java
+++ src/main/java/com/xmen/adn/pattern/VerticalPattern.java
@@ -1,11 +1,19 @@
 package com.xmen.adn.pattern;
 
-/**
- * Created by diegoazd on 9/06/15.
- */
 public class VerticalPattern {
-    public boolean findPatternInARow() {
-        return true;
+    static final byte MAX_SECUENCE_LENGTH = 4;
+    public boolean findPatternInARow(char[] row) {
+        char currentSecuence = row[0];
+        byte secuenceLength = 1;
+        for(int i=1; i<row.length;i++) {
+            if(currentSecuence == row[i] && MAX_SECUENCE_LENGTH == secuenceLength)
+               return true;
+            else {
+                currentSecuence = row[i];
+                secuenceLength = 1;
+            }
+        }
+        return false;
     }
 
 }
