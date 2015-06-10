aa0616cfc63d15ec9a568209700f6dd6e85f054f
Diego Zarate <diego.zarate@360txp.net>
2015-06-09 19:50:47 -0500

    adding horizontal search

--- src/main/java/com/xmen/adn/pattern/FindMutant.java
+++ src/main/java/com/xmen/adn/pattern/FindMutant.java
@@ -1,10 +1,33 @@
 package com.xmen.adn.pattern;
 
 public class FindMutant {
+
+    FindPattern findPattern = new FindPattern();
+
+    public boolean findMutant(char adn[][]) {
+       int arraySize = adn[0].length;
+       for(int i=0; i < arraySize; i++)
+           for(int j=0; j < arraySize; j++) {
+               if(isBetweenEdges(j,arraySize)) {
+                   char [] subAdnSequence = extractHorizontalAdnSequence(adn, j, i);
+                   byte position = findPattern.findPattern(subAdnSequence);
+                   if(position == 3)
+                       return true;
+               }
+           }
+        return false;
+    }
+
+    private boolean isBetweenEdges(int currentPosition, int arraySize) {
+        return currentPosition + 3 < arraySize;
+    }
+
     public char[] extractHorizontalAdnSequence(char[][] adn, int position, int row) {
         char[] adnSequence = new char[4];
-        for(int i = position; i < 4; i++)
-            adnSequence[i - position] = adn[row][i];
+        byte subArrayPosition = 0;
+        for(int i = position; i < 4; i++) {
+            adnSequence[subArrayPosition++] = adn[row][i];
+        }
 
         return adnSequence;
     }
--- src/test/groovy/com/xmen/adn/pattern/FindMutantSpec.groovy
+++ src/test/groovy/com/xmen/adn/pattern/FindMutantSpec.groovy
@@ -10,12 +10,75 @@ class FindMutantSpec extends spock.lang.Specification{
       adn[0][1] = 'T'
       adn[0][2] = 'C'
       adn[0][3] = 'G'
-        when:
+    when:
       char[] verticalSubArray = findMutant.
               extractHorizontalAdnSequence(adn,0,0)
-      println(verticalSubArray)
     then:
       verticalSubArray == ['A','T','C','G']
+    }
+
+    void "should find an horizontal pattern"() {
+    setup:
+      FindMutant findMutant = new FindMutant()
+     and:
+      char[][] adn = new char[6][6]
+      adn[0][0] = 'A'
+      adn[0][1] = 'T'
+      adn[0][2] = 'G'
+      adn[0][3] = 'G'
+      adn[0][4] = 'G'
+      adn[0][5] = 'G'
+    when:
+      boolean isAValidSequence = findMutant.findMutant(adn)
+    then:
+      isAValidSequence
+    }
+
+    void "should not find an horizontal pattern"() {
+        setup:
+        FindMutant findMutant = new FindMutant()
+        and:
+        char[][] adn = new char[6][6]
+        adn[0][0] = 'A'
+        adn[0][1] = 'T'
+        adn[0][2] = 'G'
+        adn[0][3] = 'C'
+        adn[0][4] = 'G'
+        adn[0][5] = 'G'
+        adn[1][0] = 'A'
+        adn[1][1] = 'T'
+        adn[1][2] = 'G'
+        adn[1][3] = 'C'
+        adn[1][4] = 'G'
+        adn[1][5] = 'G'
+        adn[2][0] = 'A'
+        adn[2][1] = 'T'
+        adn[2][2] = 'G'
+        adn[2][3] = 'C'
+        adn[2][4] = 'G'
+        adn[2][5] = 'G'
+        adn[3][0] = 'A'
+        adn[3][1] = 'T'
+        adn[3][2] = 'G'
+        adn[3][3] = 'C'
+        adn[3][4] = 'G'
+        adn[3][5] = 'G'
+        adn[4][0] = 'A'
+        adn[4][1] = 'T'
+        adn[4][2] = 'G'
+        adn[4][3] = 'C'
+        adn[4][4] = 'G'
+        adn[4][5] = 'G'
+        adn[5][0] = 'A'
+        adn[5][1] = 'T'
+        adn[5][2] = 'G'
+        adn[5][3] = 'C'
+        adn[5][4] = 'G'
+        adn[5][5] = 'G'
+        when:
+        boolean isAValidSequence = findMutant.findMutant(adn)
+        then:
+        !isAValidSequence
     }
 }
 
